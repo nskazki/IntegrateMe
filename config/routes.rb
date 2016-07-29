c020c621093bc8ca3632fc894b7de93f2ab78681
nskazki <nskazki@gmail.com>
2016-07-29 19:12:46 +0300

    replace original routes by /api/:resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  resources :entries
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-  get ':competition_id/:permalink' => 'competitions#entrant_page', constraints: {competition_id: /\d+/}
-  post 'entries' => 'entries#create'
+  resources :entries,      path: 'api/entries'
+  resources :competitions, path: 'api/competitions'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
